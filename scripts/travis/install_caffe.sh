f87436be27bc28392ef387b471453c0f3333ec1c
venky18 <mahapatra.venketesh@gmail.com>
2017-03-05 12:56:17 +0530

    clean up

--- scripts/travis/install_caffe.sh
+++ scripts/travis/install_caffe.sh
@@ -21,13 +21,7 @@ wget https://github.com/venky18/venky18.github.io/blob/master/Makefile.config
 echo "ls"
 cd python
 for req in $(cat requirements.txt); do sudo -H pip install $req --upgrade; done
-<<<<<<< HEAD
 make all -j4
 make test -j4
 make runtest -j4
 make pycaffe -j4
-=======
-make all -j2
-make test -j2
-make runtest -j2
-make pycaffe -j2
