c187947b2ec76ae2cb8c3fb5c412ef5564583017
venky18 <mahapatra.venketesh@gmail.com>
2017-03-05 13:58:19 +0530

    clean up

--- scripts/travis/install_caffe.sh
+++ scripts/travis/install_caffe.sh
@@ -13,6 +13,14 @@ sudo apt-get install -y python-numpy python-scipy
 # (OpenCV 2.4)
 sudo apt-get install -y libopencv-dev
 cd $HOME/tools
+wget https://github.com/schuhschuh/gflags/archive/master.zip
+unzip master.zip
+cd gflags-master
+mkdir build && cd build
+export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
+make 
+sudo make install
+cd $HOME/tools
 git clone https://github.com/BVLC/caffe.git
 cd caffe
 #configure cmake file
@@ -20,7 +28,6 @@ cd python
 for req in $(cat requirements.txt); do sudo -H pip install $req --upgrade; done
 cd ..
 wget https://raw.githubusercontent.com/venky18/venky18.github.io/master/Makefile.config
-cat  Makefile.config
 make all -j4
 make test -j4
 make runtest -j4
