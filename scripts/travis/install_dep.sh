56ab4b95cca167a5ac944c1f4aa2e50c7cdc9586
venky18 <mahapatra.venketesh@gmail.com>
2017-03-05 16:22:49 +0530

    23

--- scripts/travis/install_dep.sh
+++ scripts/travis/install_dep.sh
@@ -46,6 +46,23 @@ apt-get install -y --no-install-recommends \
   libopenblas-dev \
   python-virtualenv \
   wget
+# glog
+wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+tar zxvf glog-0.3.3.tar.gz
+cd glog-0.3.3
+./configure
+make && make install
+# gflags
+wget https://github.com/schuhschuh/gflags/archive/master.zip
+unzip master.zip
+cd gflags-master
+mkdir build && cd build
+export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
+make && make install
+# lmdb
+git clone https://github.com/LMDB/lmdb
+cd lmdb/libraries/liblmdb
+make && make install
 
 if $WITH_CMAKE ; then
   apt-get install -y --no-install-recommends cmake
